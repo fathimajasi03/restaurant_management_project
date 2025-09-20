ab3a5475b54e8abb6cb7db5186ed195ccb32bea7
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-20 20:19:28 +0530

    49640

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from .views import MenuCategoryListView
 
-"""
-URL patterns for the home app including the menu categories API endpoint.
-"""
-
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-category-list'),
     ]
-    
