c520d7c39ef0147692af1202e735f674a4548f7a
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-20 19:30:07 +0530

    49636

--- manage.py
+++ manage.py
@@ -1,22 +1,8 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from django.db import models
 
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+        def __str__(self):
+                return self.name
+                
