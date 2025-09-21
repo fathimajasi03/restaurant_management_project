97a9df02263d7000b3ecc758c6c63e9ad6027470
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-21 20:54:15 +0530

    49884

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuItemViewSet
+from.views import MenuItemViewSet
 
 router = DefaultRouter()
 router.register('menu-items', MenuItemViewSet, basename='menuitem')
-
+from django.urls import path, include
 urlpatterns = [
     path('api/', include(home.urls)),
     ]
