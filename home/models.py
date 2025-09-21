d5e2d6bfd449b45d4e4c7a975d858babb21feb2e
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-21 20:36:04 +0530

    49884

--- home/models.py
+++ home/models.py
@@ -1,4 +1,3 @@
-# models.py (example)
 from django.db import models
 
 class MenuItem(models.Model):
@@ -7,17 +6,6 @@ class MenuItem(models.Model):
             price = models.DecimalField(max_digits=6, decimal_places=2)
                 available = models.BooleanField(default=True)
 
-                # serializers.py
-                from rest_framework import serializers
-                from .models import MenuItem
-
-                class MenuItemSerializer(serializers.ModelSerializer):
-                    class Meta:
-                            model = MenuItem
-                                    fields = ['id', 'name', 'description', 'price', 'available']
-
-                                        # Validate price to be positive
-                                            def validate_price(self, value):
-                                                    if value < 0:
-                                                                raise serializers.ValidationError("Price must be a positive number.")
-                                                                        return value
+                    def __str__(self):
+                            return self.name
+                            
