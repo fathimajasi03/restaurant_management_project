24fd81544218b35abd803bce7670e40405dada2e
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-21 20:38:25 +0530

    49884

--- home/views.py
+++ home/views.py
@@ -1,16 +1,9 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.filters import SearchFilter
+from rest_framework import viewsets, permissions
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
-class MenuItemPagination(PageNumberPagination):
-    page_size = 10  # You can set this as needed
-
-    class MenuItemViewSet(ReadOnlyModelViewSet):
-        queryset = MenuItem.objects.all()
-            serializer_class = MenuItemSerializer
-                pagination_class = MenuItemPagination
-                    filter_backends = [SearchFilter]
-                        search_fields = ['name']
-                        
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+        serializer_class = MenuItemSerializer
+            permission_classes = [permissions.IsAdminUser]  # Only admins can update
+            
