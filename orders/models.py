381ea858cf3d4b70b9c77c0ad286f359dabddd78
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-24 19:40:46 +0530

    50391

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,15 @@
- from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
- class Order(models.Model):
-     order_id = models.CharField(max_length=16, unique=True)
-         # other fields...
-                        
+class Order(models.Model):
+    STATUS_CHOICES = [
+            ('Pending', 'Pending'),
+                    ('Processing', 'Processing'),
+                            ('Cancelled', 'Cancelled'),
+                                    ('Completed', 'Completed'),
+                                        ]
 
-                                              
+                                            user = models.ForeignKey(User, on_delete=models.CASCADE)
+                                                status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+                                                    # Other fields ...
+                                                    
