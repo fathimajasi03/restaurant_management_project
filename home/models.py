24326be47ab9d1b33794d9ffc5bba447ce7ca754
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-25 22:15:48 +0530

    51085

--- home/models.py
+++ home/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-class ContactFormSubmission(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=255)
-        email = models.EmailField()
-            message = models.TextField()
-
-                def __str__(self):
-                        return f"Contact from {self.name} <{self.email}>"
-                        
+        price = models.DecimalField(max_digits=6, decimal_places=2)
+            is_daily_special = models.BooleanField(default=False)
+                # other existing fields
+                
