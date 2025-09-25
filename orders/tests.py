713bbc9f11d7326b65cf11f21a4e677f6cfe38e4
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-25 17:17:25 +0530

    51084

--- orders/tests.py
+++ orders/tests.py
@@ -1,23 +1,20 @@
- from django.test import TestCase
- from decimal import Decimal
- from orders.models import Order, OrderItem
- from home.models import MenuItem
- from django.contrib.auth.models import User
+from django.test import TestCase
+from decimal import Decimal
+from orders.models import Order, OrderItem
+from home.models import MenuItem
+from django.contrib.auth.models import User
 
- class OrderModelTest(TestCase):
-     def setUp(self):
-             user = User.objects.create_user(username='tester', password='pass')
-                     self.menu_item = MenuItem.objects.create(name='Burger', price=Decimal('5.00'))
-                             self.order = Order.objects.create(user=user)
+class OrderTotalTest(TestCase):
+    def setUp(self):
+            user = User.objects.create(username="tester")
+                    order = Order.objects.create(user=user)
+                            product = MenuItem.objects.create(name="Pizza", price=Decimal("10.00"))
+                                    OrderItem.objects.create(order=order, menu_item=product, quantity=2, price=Decimal("10.00"))
+                                            OrderItem.objects.create(order=order, menu_item=product, quantity=3, price=Decimal("10.00"))
+                                                    self.order = order
 
-                                 def test_calculate_total(self):
-                                         OrderItem.objects.create(
-                                                     order=self.order, menu_item=self.menu_item, quantity=2, price=Decimal('5.00')
-                                                             )
-                                                                     OrderItem.objects.create(
-                                                                                 order=self.order, menu_item=self.menu_item, quantity=1, price=Decimal('5.00')
-                                                                                         )
-
-                                                                                                 total = self.order.calculate_total()
-                                                                                                         self.assertEqual(total, Decimal('15.00'))
-                                                                                                         
+                                                        def test_calculate_total(self):
+                                                                total = self.order.calculate_total()
+                                                                        # first line: 2 * 10.00 = 20.00, second line: 3 * 9.00 = 27.00, grand total = 47.00
+                                                                                self.assertEqual(total, Decimal("47.00"))
+                                                                                
