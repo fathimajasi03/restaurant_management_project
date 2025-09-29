f0cf7da5eecfbbfc6c2488cb0b225b61c7dc4e9d
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-29 20:55:36 +0530

    52204

--- home/models.py
+++ home/models.py
@@ -1,14 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User  # Or your custom user model
-from .models import MenuItem  # Adjust import if MenuItem is elsewhere
 
-class UserReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
-        menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
-            rating = models.IntegerField()
-                comment = models.TextField()
-                    review_date = models.DateTimeField(auto_now_add=True)
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-                        def __str__(self):
-                                return f"Review by {self.user.username} on {self.menu_item.name} ({self.rating} stars)"
-                                
+        def __str__(self):
+                return self.name
+                
