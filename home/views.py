b49ce6d5855f6d3192e0aeed268464900eb8b188
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-29 20:58:03 +0530

    52204

--- home/views.py
+++ home/views.py
@@ -1,19 +1,8 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import UserReview
-from .serializers import UserReviewSerializer
+from rest_framework import viewsets
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
 
-class UserReviewCreateView(CreateAPIView):
-    serializer_class = UserReviewSerializer
-        permission_classes = [IsAuthenticatedOrReadOnly]
-
-            def perform_create(self, serializer):
-                    serializer.save(user=self.request.user)
-
-                    class MenuItemReviewsListView(ListAPIView):
-                        serializer_class = UserReviewSerializer
-
-                            def get_queryset(self):
-                                    menu_item_id = self.kwargs['menu_item_id']
-                                            return UserReview.objects.filter(menu_item_id=menu_item_id)
-                                            
+class MenuCategoryViewSet(viewsets.ModelViewSet):
+    queryset = MenuCategory.objects.all()
+        serializer_class = MenuCategorySerializer
+        (
