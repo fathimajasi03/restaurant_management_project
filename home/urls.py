ff35ae798ba6a00a44c9a05a86c9c58bd20c8583
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-09-30 19:12:30 +0530

    52465

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import RestaurantDetailAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MenuCategoryViewSet
+
+router = DefaultRouter()
+router.register(r'menu-categories', MenuCategoryViewSet, basename='menu-category')
 
 urlpatterns = [
-    path('restaurants/<int:pk>/', RestaurantDetailAPIView.as_view(), name='restaurant-detail'),
+    path('', include(router.urls)),
     ]
     
