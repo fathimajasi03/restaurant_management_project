c0f65beef9234b44196b35eb2e02d8d48f875462
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-04 18:49:55 +0530

    53382

--- home/views.py
+++ home/views.py
@@ -1,19 +1,7 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import MenuItem
-from .serializers import MenuItemAvailabilitySerializer
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
 
-@api_view(['PATCH'])
-def update_menu_item_availability(request, pk):
-    try:
-            menu_item = MenuItem.objects.get(pk=pk)
-                except MenuItem.DoesNotExist:
-                        return Response({"error": "Menu item not found."}, status=status.HTTP_404_NOT_FOUND)
-
-                            serializer = MenuItemAvailabilitySerializer(menu_item, data=request.data, partial=True)
-                                if serializer.is_valid():
-                                        serializer.save()
-                                                return Response({"message": "Availability updated successfully.", "data": serializer.data})
-                                                    else:
-                                                            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TableDetailView(generics.RetrieveAPIView):
+    queryset = Table.objects.all()
+        serializer_class = TableSerializer
