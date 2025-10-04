d6c8d38cdf8b8a971e995dd5fa6b6a1510a43489
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-04 18:56:26 +0530

    53383

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from .models import Table
 from .serializers import TableSerializer
 
-class TableDetailView(generics.RetrieveAPIView):
-    queryset = Table.objects.all()
-        serializer_class = TableSerializer
+class AvailableTablesAPIView(generics.ListAPIView):
+    serializer_class = TableSerializer
+
+        def get_queryset(self):
+                return Table.objects.filter(is_available=True)
