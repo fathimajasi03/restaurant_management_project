07c63ad271315cb0677ff626ec415ff8cd538167
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-04 18:56:55 +0530

    53383

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import TableDetailView
+from .views import AvailableTablesAPIView
 
 urlpatterns = [
-    path('api/tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
+    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
     ]
