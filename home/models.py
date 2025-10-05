395f0315f2a3a0aed0dfa408768827b45001c5ae
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-05 16:05:16 +0530

    53523

--- home/models.py
+++ home/models.py
@@ -1,16 +1,7 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-        price = models.DecimalField(max_digits=8, decimal_places=2)
-            discount_percentage = models.DecimalField(
-                    max_digits=4, decimal_places=2, default=0.00,
-                            help_text="Discount as a decimal fraction, e.g. 0.10 for 10% off"
-                                )
-                                    # other relevant fields ...
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-                                        def get_final_price(self):
-                                                discount = float(self.discount_percentage)
-                                                        original_price = float(self.price)
-                                                                final_price = original_price * (1 - discount)
-                                                                        return max(final_price, 0.0)
+        def __str__(self):
+                return self.name
