c2eb22e3054f156b61e2b0cd08fc926d3c926045
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-06 19:11:28 +0530

    53642

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,4 @@
-  from django.db import models
-  
-  class Coupon(models.Model):
-      code = models.CharField(max_length=64, unique=True)
-          discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-              is_active = models.BooleanField(default=True)
-                  valid_from = models.DateField()
-                      valid_until = models.DateField()
-                      
-                          def __str__(self):
-                                  return self.code                                                                                                                                                                                                                      order = models.ForeignKey(Or                
+from django.db import models
+class Order(models.Model):
+    unique_id = models.CharField(max_length=16, unique=True)
+        status = models.CharField(max_length=20)
