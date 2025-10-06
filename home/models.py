c49881fbc6a7afae7895112a04fb6ee0d421555d
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-06 19:06:42 +0530

    53645

--- home/models.py
+++ home/models.py
@@ -1,18 +1,4 @@
 from django.db import models
-
-class OpeningHours(models.Model):
-    DAY_CHOICES = [
-            ('Monday', 'Monday'),
-                    ('Tuesday', 'Tuesday'),
-                            ('Wednesday', 'Wednesday'),
-                                    ('Thursday', 'Thursday'),
-                                            ('Friday', 'Friday'),
-                                                    ('Saturday', 'Saturday'),
-                                                            ('Sunday', 'Sunday'),
-                                                                ]
-                                                                    day = models.CharField(max_length=10, choices=DAY_CHOICES, unique=True)
-                                                                        open_time = models.TimeField()
-                                                                            close_time = models.TimeField()
-
-                                                                                def __str__(self):
-                                                                                        return f"{self.day}: {self.open_time} - {self.close_time}"
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+        image = models.URLField(blank=True)
