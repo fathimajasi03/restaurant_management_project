de5380e919fd261957f238d10de66a71bed1ec26
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-06 19:12:41 +0530

    53642

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,14 +1,6 @@
-       from rest_framework import serializers
-       from .models import Order
-
-       class OrderStatusUpdateSerializer(serializers.ModelSerializer):
-           class Meta:
-                   model = Order
-                           fields = ['status']
-
-                               def validate_status(self, value):
-                                       allowed_statuses = [choice[0] for choice in Order.STATUS_CHOICES]
-                                               if value not in allowed_statuses:
-                                                           raise serializers.ValidationError("Invalid status.")
-                                                                   return value
-                                                                                                    
+from rest_framework import serializers
+from .models import Order
+class OrderStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Order
+                    fields = ['status']
