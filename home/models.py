50efe6a6b35a584e647cbe3ac09cb5ccc90e0d18
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-06 19:01:42 +0530

    53641

--- home/models.py
+++ home/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class OpeningHours(models.Model):
+    DAY_CHOICES = [
+            ('Monday', 'Monday'),
+                    ('Tuesday', 'Tuesday'),
+                            ('Wednesday', 'Wednesday'),
+                                    ('Thursday', 'Thursday'),
+                                            ('Friday', 'Friday'),
+                                                    ('Saturday', 'Saturday'),
+                                                            ('Sunday', 'Sunday'),
+                                                                ]
+                                                                    day = models.CharField(max_length=10, choices=DAY_CHOICES, unique=True)
+                                                                        open_time = models.TimeField()
+                                                                            close_time = models.TimeField()
 
-        def __str__(self):
-                return self.name
+                                                                                def __str__(self):
+                                                                                        return f"{self.day}: {self.open_time} - {self.close_time}"
