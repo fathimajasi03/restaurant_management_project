d48eec2443db7102d31aa8342698874d26f6a522
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-06 19:08:10 +0530

    53645

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import ListAPIView
-from .models import OpeningHours
-from .serializers import OpeningHoursSerializer
-
-class OpeningHoursListAPIView(ListAPIView):
-    queryset = OpeningHours.objects.all().order_by('id')
-        serializer_class = OpeningHoursSerializer
+from .models import MenuItem
+from .serializers import MenuItemSearchSerializer
+class MenuItemSearchAPIView(ListAPIView):
+    serializer_class = MenuItemSearchSerializer
+        def get_queryset(self):
+                query = self.request.GET.get('q')
+                        if query:
+                                    return MenuItem.objects.filter(name__icontains=query)
+                                            return MenuItem.objects.none()
