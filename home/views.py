1242a0402d649902d7a839b52f4f1481852fe304
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-07 19:40:51 +0530

    53910

--- home/views.py
+++ home/views.py
@@ -1,9 +1,6 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Restaurant
-
-class RestaurantOpeningHoursAPIView(APIView):
-    def get(self, request):
-            restaurant = Restaurant.objects.first()
-                    opening_hours = restaurant.opening_hours if restaurant else "Not available"
-                            return Response({"opening_hours": opening_hours})
+from rest_framework.generics import ListAPIView
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+class MenuItemListAPIView(ListAPIView):
+    queryset = MenuItem.objects.all()
+        serializer_class = MenuItemSerializer
