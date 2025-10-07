92d30b6eadc0ad9b7894b04c0b07522044880206
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-07 19:44:00 +0530

    53911

--- home/views.py
+++ home/views.py
@@ -1,6 +1,30 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import MenuItem
 from .serializers import MenuItemSerializer
-class MenuItemListAPIView(ListAPIView):
-    queryset = MenuItem.objects.all()
-        serializer_class = MenuItemSerializer
+
+class MenuItemPriceRangeAPIView(ListAPIView):
+    serializer_class = MenuItemSerializer
+
+        def get_queryset(self):
+                queryset = MenuItem.objects.all()
+                        min_price = self.request.query_params.get('min_price')
+                                max_price = self.request.query_params.get('max_price')
+                                        try:
+                                                    if min_price is not None:
+                                                                    min_price = float(min_price)
+                                                                                    queryset = queryset.filter(price__gte=min_price)
+                                                                                                if max_price is not None:
+                                                                                                                max_price = float(max_price)
+                                                                                                                                queryset = queryset.filter(price__lte=max_price)
+                                                                                                                                        except ValueError:
+                                                                                                                                                    return MenuItem.objects.none()
+                                                                                                                                                            return queryset
+
+                                                                                                                                                                def list(self, request, *args, **kwargs):
+                                                                                                                                                                        queryset = self.get_queryset()
+                                                                                                                                                                                if queryset is None:
+                                                                                                                                                                                            return Response({"error": "Invalid price parameters"}, status=status.HTTP_400_BAD_REQUEST)
+                                                                                                                                                                                                    serializer = self.get_serializer(queryset, many=True)
+                                                                                                                                                                                                            return Response(serializer.data)
