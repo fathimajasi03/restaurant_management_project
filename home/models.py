fa23b2a7c3c951442456c04fdaad62b21eea2f0f
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-07 19:35:45 +0530

    53909

--- home/models.py
+++ home/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 class Restaurant(models.Model):
     # existing fields ...
-
-        def get_total_menu_items(self):
-                from .models import MenuItem
-                        return MenuItem.objects.count()
+        opening_hours = models.CharField(max_length=50, default="11:00 AM - 11:00 PM (EST)")
