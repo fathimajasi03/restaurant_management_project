c3ae6d574eb7a7a037277dab121e71892f3b0381
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-08 20:47:56 +0530

    54120

--- home/models.py
+++ home/models.py
@@ -6,3 +6,6 @@ class MenuCategory(models.Model):
             price = models.DecimalField(max_digits=8,decimal_places=2)
                 description = models.TextField()
                     category = models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
+                    @classmethod
+                    def filter_by_cuisine(cls,cuisine_type):
+                     return cls.objects.filter(cuisine_type=cuisine_type)
