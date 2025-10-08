7a28ce91db55c0a744707d5cebc026f2a0cb94cb
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-08 20:55:20 +0530

    54116

--- home/models.py
+++ home/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100)
-    class MenuItem(models.Model):
-        name = models.CharField(max_length=100)
-            price = models.DecimalField(max_digits=8,decimal_places=2)
-                description = models.TextField()
-                    category = models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
-                    @classmethod
-                    def filter_by_cuisine(cls,cuisine_type):
-                     return cls.objects.filter(cuisine_type=cuisine_type)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+        is_available = models.BooleanField(default=True)
+            # other fields...
