92edae4723b805e57a612aa03784239add9945fb
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-09 21:49:07 +0530

    54287

--- home/models.py
+++ home/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=255)
-        is_available = models.BooleanField(default=True)
-            # other fields...
+        price = models.DecimalField(max_digits=8, decimal_places=2)
+            available = models.BooleanField(default=True)  # Availability field
+
+                def __str__(self):
+                        return self.name
