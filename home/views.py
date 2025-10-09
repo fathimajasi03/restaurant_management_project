7ae3e9dab76dc9a0f2170ed6f1dd9bf8b8da73a7
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-09 21:51:19 +0530

    54287

--- home/views.py
+++ home/views.py
@@ -1,23 +1,9 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
-from rest_framework import status
-from .models import UserReview
-from .serializers import ReviewSerializer
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-class ReviewPagination(PageNumberPagination):
-    page_size = 10
-        page_size_query_param = 'page_size'
-            max_page_size = 50
+class AvailableMenuItemsAPIView(ListAPIView):
+    serializer_class = MenuItemSerializer
 
-            class ReviewListAPIView(ListAPIView):
-                queryset = UserReview.objects.all().order_by('-created_at')  # Order newest first
-                    serializer_class = ReviewSerializer
-                        pagination_class = ReviewPagination
-
-                            def list(self, request, *args, **kwargs):
-                                    try:
-                                                return super().list(request, *args, **kwargs)
-                                                        except Exception as e:
-                                                                    return Response({'error': 'An error occurred while fetching reviews.'},
-                                                                                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        def get_queryset(self):
+                return MenuItem.objects.filter(available=True)
