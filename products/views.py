3a556b376c57d81fe66d0e76dd1151e2653ab75f
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-10 22:04:26 +0530

    54290

--- products/views.py
+++ products/views.py
@@ -1,11 +1,18 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from .models import Order
-from .serializers import OrderSerializer
+from .serializers import OrderHistorySerializer
 
-class OrderDetailAPIView(RetrieveAPIView):
-    queryset = Order.objects.all()
-        serializer_class = OrderSerializer
-            permission_classes = [IsAuthenticated]
-                lookup_field = 'id'
-                
+class OrderHistoryPagination(PageNumberPagination):
+    page_size = 10
+        page_size_query_param = 'page_size'
+            max_page_size = 50
+
+            class UserOrderHistoryAPIView(ListAPIView):
+                serializer_class = OrderHistorySerializer
+                    permission_classes = [IsAuthenticated]
+                        pagination_class = OrderHistoryPagination
+
+                            def get_queryset(self):
+                                    return Order.objects.filter(user=self.request.user).order_by('-created_at')
