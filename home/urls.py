d4a70fdb659d04b3f1406843375643af0b720736
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-13 23:29:22 +0530

    54829

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import FAQListAPIView
+from .views import AvailableMenuItemCountAPIView
 
 urlpatterns = [
-    path('faqs/', FAQListAPIView.as_view(), name='faq-list'),
+    path('menu-items/available/count/', AvailableMenuItemCountAPIView.as_view(), name='available-menu-items-count'),
     ]
