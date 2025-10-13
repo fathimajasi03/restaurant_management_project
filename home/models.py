1c6f61a601d481af14ba44723ebe3fe5395319b8
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-13 23:28:09 +0530

    54829

--- home/models.py
+++ home/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class FAQ(models.Model):
-    question = models.CharField(max_length=255)
-        answer = models.TextField()
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+        price = models.DecimalField(max_digits=8, decimal_places=2)
+            is_available = models.BooleanField(default=True)
 
-            def __str__(self):
-                    return self.question
+                def __str__(self):
+                        return self.name
