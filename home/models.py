6f0f9bcf9c3af1ec740afc7dbbbfb62f58146dec
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-15 22:33:05 +0530

    55263

--- home/models.py
+++ home/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+        description = models.TextField()
+            price = models.DecimalField(max_digits=8, decimal_places=2)
+                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+                    category = models.ForeignKey('MenuCategory', on_delete=models.CASCADE, related_name='menu_items')
 
-        def __str__(self):
-                return self.name
+                        def __str__(self):
+                                return f"{self.name} - {self.category.name}"
