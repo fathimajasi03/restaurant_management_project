0e2d216fce726d98d23eb50b2701520c432bb2d1
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-19 21:14:47 +0530

    55796

--- home/models.py
+++ home/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-class Table(models.Model):
-    table_number = models.IntegerField(unique=True)
-        capacity = models.IntegerField()
-            is_available = models.BooleanField(default=True)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+        capacity = models.IntegerField(default=0, null=True, blank=True)
 
-                def __str__(self):
-                        return f"Table {self.table_number} (Capacity: {self.capacity})"
+            def __str__(self):
+                    return f"{self.name} (Capacity: {self.capacity})"
