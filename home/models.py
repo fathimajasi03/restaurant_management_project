5f669e0fe1bb8d5a43cf85a3094ea32c4b00d570
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-19 21:18:28 +0530

    55797

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
-        capacity = models.IntegerField(default=0, null=True, blank=True)
+        description = models.TextField(blank=True, null=True)
 
             def __str__(self):
-                    return f"{self.name} (Capacity: {self.capacity})"
+                    return self.name
