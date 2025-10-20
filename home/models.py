d838faedc07e9a84386113e2b55e4f43d963fb24
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-20 22:02:18 +0530

    55860

--- home/models.py
+++ home/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=200)
-        description = models.TextField(blank=True, null=True)
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-            def __str__(self):
-                    return self.name
+        def __str__(self):
+                return self.name
+
+
+                class MenuItem(models.Model):
+                    name = models.CharField(max_length=100)
+                        description = models.TextField()
+                            price = models.DecimalField(max_digits=8, decimal_places=2)
+                                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+                                    category = models.ForeignKey('MenuCategory', on_delete=models.CASCADE, related_name='menu_items')
+
+                                        # New ForeignKey field to Cuisine
+                                            cuisine = models.ForeignKey(
+                                                    Cuisine,
+                                                            on_delete=models.SET_NULL,
+                                                                    null=True,
+                                                                            blank=True,
+                                                                                    related_name='menu_items'
+                                                                                        )
+
+                                                                                            def __str__(self):
+                                                                                                    return f"{self.name} ({self.cuisine.name if self.cuisine else 'No Cuisine'})"
