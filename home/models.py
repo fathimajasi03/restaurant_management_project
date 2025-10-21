cfe438410edeb128ae9ceae737e237713e25d513
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-21 19:45:15 +0530

    55861

--- home/models.py
+++ home/models.py
@@ -5,23 +5,3 @@ class Cuisine(models.Model):
 
         def __str__(self):
                 return self.name
-
-
-                class MenuItem(models.Model):
-                    name = models.CharField(max_length=100)
-                        description = models.TextField()
-                            price = models.DecimalField(max_digits=8, decimal_places=2)
-                                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-                                    category = models.ForeignKey('MenuCategory', on_delete=models.CASCADE, related_name='menu_items')
-
-                                        # New ForeignKey field to Cuisine
-                                            cuisine = models.ForeignKey(
-                                                    Cuisine,
-                                                            on_delete=models.SET_NULL,
-                                                                    null=True,
-                                                                            blank=True,
-                                                                                    related_name='menu_items'
-                                                                                        )
-
-                                                                                            def __str__(self):
-                                                                                                    return f"{self.name} ({self.cuisine.name if self.cuisine else 'No Cuisine'})"
