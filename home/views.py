95577a338d1c243f20afe16272fc052663209dad
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-23 21:20:28 +0530

    56195

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
-from rest_framework.generics import ListAPIView
-from .models import Cuisine
-from .serializers import CuisineSerializer
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import MenuItem
+from .serializers import MenuItemIngredientListSerializer
 
-class CuisineListView(ListAPIView):
-    queryset = Cuisine.objects.all()
-        serializer_class = CuisineSerializer
+class MenuItemIngredientsView(RetrieveAPIView):
+    queryset = MenuItem.objects.all()
+        serializer_class = MenuItemIngredientListSerializer
+
+            def get(self, request, *args, **kwargs):
+                    try:
+                                menu_item = self.get_object()
+                                        except MenuItem.DoesNotExist:
+                                                    return Response({'detail': 'MenuItem not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+                                                            serializer = self.get_serializer(menu_item)
+                                                                    return Response(serializer.data)
