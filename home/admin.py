fd1da7d7d7f369d0b08ada037eddf8718d5a7b1b
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-24 19:33:30 +0530

    56197

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,11 @@
+# home/admin.py
+
 from django.contrib import admin
 from .models import Restaurant
 
-admin.site.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone_number', 'email')
+        search_fields = ('name', 'address')
+            list_filter = ('is_active',)  # Add only if Restaurant model has 'is_active' BooleanField
+
+            admin.site.register(Restaurant, RestaurantAdmin)
