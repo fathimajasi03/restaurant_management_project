fe00bdaeab73ca52e837481b135fda46c53a0c60
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-28 23:50:29 +0530

    56686

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,4 @@
-# home/admin.py
-
 from django.contrib import admin
-from .models import Restaurant
+from .models import DailyOperatingHours
 
-class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'phone_number', 'email')
-        search_fields = ('name', 'address')
-            list_filter = ('is_active',)  # Add only if Restaurant model has 'is_active' BooleanField
-
-            admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(DailyOperatingHours)
