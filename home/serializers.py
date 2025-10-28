77a35d3fbdf5a2dd57dd886d158486308740f5ff
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-10-28 23:46:25 +0530

    56685

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import MenuItem
 
-class FeaturedMenuItemSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
             model = MenuItem
-                    fields = ['id', 'name', 'price', 'description', 'is_featured']  # include fields you want
+                    fields = ['id', 'name', 'description', 'price', 'is_featured']
