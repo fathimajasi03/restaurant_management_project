22f4869f3aadc49762ee8091d2327559b5e92906
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-06 20:20:05 +0530

    58439

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import menuitem_list_create, menuitem_detail
+from .views import RestaurantDetailAPIView
 
 urlpatterns = [
-    path('menu-items/', menuitem_list_create, name='menuitem-list-create'),
-        path('menu-items/<int:pk>/', menuitem_detail, name='menuitem-detail'),
-        ]
+    path('api/restaurant/<int:pk>/', RestaurantDetailAPIView.as_view(), name='restaurant-detail'),
+    ]
