22a4b8096b20253f9f4ce2a5c408f633cc49db28
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-11 08:18:28 -0800

    60249

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
-from rest_framework.generics import RetrieveAPIView
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import MenuItem
+from .serializers import MenuItemAvailabilitySerializer
 
-class RestaurantDetailAPIView(RetrieveAPIView):
-    queryset = Restaurant.objects.all()
-        serializer_class = RestaurantSerializer
+@api_view(['PATCH'])
+def update_menu_item_availability(request, pk):
+    try:
+            menu_item = MenuItem.objects.get(pk=pk)
+                except MenuItem.DoesNotExist:
+                        return Response({"error": "Menu item not found."}, status=status.HTTP_404_NOT_FOUND)
+
+                            serializer = MenuItemAvailabilitySerializer(menu_item, data=request.data, partial=True)
+                                if serializer.is_valid():
+                                        serializer.save()
+                                                return Response({"message": "Availability updated successfully.", "data": serializer.data})
+                                                    else:
+                                                            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
