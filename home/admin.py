3c3add8d8f7b640ae87ff178576d14fbeac751b2
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-11 08:25:10 -0800

    60252

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import DailyOperatingHours
+from .models import MenuItem
 
-admin.site.register(DailyOperatingHours)
+def make_unavailable(modeladmin, request, queryset):
+    for item in queryset:
+            item.is_available = False
+                    item.save()
+                    make_unavailable.short_description = "Mark selected menu items as unavailable"
+
+                    @admin.register(MenuItem)
+                    class MenuItemAdmin(admin.ModelAdmin):
+                        list_display = ['name', 'is_available', 'price']  # adjust fields as needed
+                            actions = [make_unavailable]
