d7da11f44b94b073380aceb701c02e97f5b2fccf
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-12 09:49:11 -0800

    60407

--- home/models.py
+++ home/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-class Customer(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True)
-        phone = models.CharField(max_length=20, blank=True, null=True)
-            email = models.EmailField(blank=True, null=True)
-                created_at = models.DateTimeField(auto_now_add=True)
+class Table(models.Model):
+    # ... existing fields ...
+        max_seats = models.IntegerField(default=4)  # mandatory, default to 4 seats
 
-                    def __str__(self):
-                            return self.name if self.name else f"Customer {self.id}"
+            # ... any other methods or fields ...
