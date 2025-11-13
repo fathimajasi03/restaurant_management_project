035746635ecbb4e61c9f9ff6ecb8fff130b0605b
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-13 08:31:20 -0800

    60409

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import UserLoyaltySerializer
 
-# Create your views here.
+class MyLoyaltyPointsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+        def get(self, request):
+                serializer = UserLoyaltySerializer(request.user)
+                        return Response(serializer.data)g
