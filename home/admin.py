1d2813f785ce52e214745454cc2088ebc4d26102
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-13 08:25:01 -0800

    60410

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,2 @@
-from django.contrib import admin
-from .models import MenuItem
-
-def make_unavailable(modeladmin, request, queryset):
-    for item in queryset:
-            item.is_available = False
-                    item.save()
-                    make_unavailable.short_description = "Mark selected menu items as unavailable"
-
-                    @admin.register(MenuItem)
-                    class MenuItemAdmin(admin.ModelAdmin):
-                        list_display = ['name', 'is_available', 'price']  # adjust fields as needed
-                            actions = [make_unavailable]
+class MenuItemAdmin(admin.ModelAdmin):
+        list_display = ['name', 'is_available', 'is_gluten_free']  # add other fields as needed
