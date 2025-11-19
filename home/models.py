fecd4b581f7a46d169ca87ebc84c96ca98416a3e
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-19 08:37:31 -0800

    60823

--- home/models.py
+++ home/models.py
@@ -1,8 +1,29 @@
+# home/models.py
 from django.db import models
 
-class MenuItem(models.Model):
-    # ... existing fields ...
-        is_gluten_free = models.BooleanField(
-                default=False,
-                        help_text='Indicates if the menu item is gluten-free.'
-                            )
+class DailyOperatingHours(models.Model):
+    MONDAY = 'Monday'
+        TUESDAY = 'Tuesday'
+            WEDNESDAY = 'Wednesday'
+                THURSDAY = 'Thursday'
+                    FRIDAY = 'Friday'
+                        SATURDAY = 'Saturday'
+                            SUNDAY = 'Sunday'
+
+                                DAY_CHOICES = [
+                                        (MONDAY, 'Monday'),
+                                                (TUESDAY, 'Tuesday'),
+                                                        (WEDNESDAY, 'Wednesday'),
+                                                                (THURSDAY, 'Thursday'),
+                                                                        (FRIDAY, 'Friday'),
+                                                                                (SATURDAY, 'Saturday'),
+                                                                                        (SUNDAY, 'Sunday'),
+                                                                                            ]
+
+                                                                                                restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+                                                                                                    day_of_week = models.CharField(max_length=9, choices=DAY_CHOICES)
+                                                                                                        opening_time = models.TimeField()
+                                                                                                            closing_time = models.TimeField()
+
+                                                                                                                def __str__(self):
+                                                                                                                        return f"{self.restaurant} - {self.day_of_week}: {self.opening_time} to {self.closing_time}"
