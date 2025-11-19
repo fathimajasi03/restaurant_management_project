60b09659d364b7099221f35d90467c0550acc518
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-19 08:43:02 -0800

    60825

--- account/models.py
+++ account/models.py
@@ -1,11 +1,13 @@
 # account/models.py
 from django.db import models
-from django.conf import settings
 
 class CustomerProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
         phone_number = models.CharField(max_length=20)
             delivery_address = models.TextField()
 
-                def __str__(self):
-                        return f"Profile of {self.user.username}"
+                def get_full_name(self):
+                        first_name = self.user.first_name or ''
+                                last_name = self.user.last_name or ''
+                                        full_name = f"{first_name} {last_name}".strip()
+                                                return full_name if full_name else ''
