56e86ed37f472336474feef426cda86c2674a6ab
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-21 08:46:28 -0800

    60906

--- home/models.py
+++ home/models.py
@@ -1,13 +1,6 @@
 # home/models.py
 from django.db import models
 
-class MenuItemManager(models.Manager):
-    def get_budget_items(self, max_price):
-            return self.filter(price__lt=max_price)
-
-            class MenuItem(models.Model):
-                name = models.CharField(max_length=255)
-                    price = models.DecimalField(max_digits=8, decimal_places=2)
-                        # other fields ...
-
-                            objects = MenuItemManager()
+class Restaurant(models.Model):
+    # ... your existing fields ...
+        max_capacity = models.IntegerField(null=True, blank=True)
