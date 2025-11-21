382294cda65d69c8350a89d8fa2987d431b0bd35
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-21 08:44:11 -0800

    60904

--- home/models.py
+++ home/models.py
@@ -1,13 +1,13 @@
 # home/models.py
 from django.db import models
-from django.conf import settings
 
-class Feedback(models.Model):
-    # ... existing fields ...
-        user = models.ForeignKey(
-                settings.AUTH_USER_MODEL,
-                        on_delete=models.SET_NULL,
-                                null=True,
-                                        blank=True
-                                            )
-                                                # ... any other fields and methods ...
+class MenuItemManager(models.Manager):
+    def get_budget_items(self, max_price):
+            return self.filter(price__lt=max_price)
+
+            class MenuItem(models.Model):
+                name = models.CharField(max_length=255)
+                    price = models.DecimalField(max_digits=8, decimal_places=2)
+                        # other fields ...
+
+                            objects = MenuItemManager()
