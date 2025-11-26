19c7a78ca1dd810bc616524a1b5b2dea983a60e2
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-26 07:23:46 -0800

    61224

--- home/models.py
+++ home/models.py
@@ -1,6 +1,19 @@
 # home/models.py
 from django.db import models
 
-class Restaurant(models.Model):
-    # ... your existing fields ...
-        max_capacity = models.IntegerField(null=True, blank=True)
+class MenuItemManager(models.Manager):
+    def get_available_items(self):
+            return self.filter(is_available=True)
+            
+            class MenuItem(models.Model):
+                name = models.CharField(max_length=255)
+                    price = models.DecimalField(max_digits=8, decimal_places=2)
+                        is_available = models.BooleanField(default=True)
+                            # other fields...
+                            
+                                objects = MenuItemManager()
+                                
+                                    # Alternative class method approach:
+                                        # @classmethod
+                                            # def get_available_items(cls):
+                                                #     return cls.objects.filter(is_available=True)                       on_delete=mode                                      blank=
