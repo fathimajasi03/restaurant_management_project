6abb28258a47b0133abc163e93fb38b76374fddc
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-28 09:23:43 -0800

    61308

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
 # home/admin.py
 from django.contrib import admin
-from .models import DailySpecial
+from .models import MenuItem, Ingredient
 
-@admin.register(DailySpecial)
-class DailySpecialAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_available', 'date')
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+    @admin.register(MenuItem)
+    class MenuItemAdmin(admin.ModelAdmin):
+        list_display = ('name', 'price', 'is_available')
+            filter_horizontal = ('ingredients',)
