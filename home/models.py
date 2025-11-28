09b150b775fceb775d7de3057d146164684fb04f
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-28 09:25:53 -0800

    61309

--- home/models.py
+++ home/models.py
@@ -1,18 +1,6 @@
 # home/models.py
 from django.db import models
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=255)
-        description = models.TextField()
-
-            def __str__(self):
-                    return self.name
-
-                    class MenuItem(models.Model):
-                        name = models.CharField(max_length=255)
-                            price = models.DecimalField(max_digits=8, decimal_places=2)
-                                is_available = models.BooleanField(default=True)
-                                    ingredients = models.ManyToManyField(Ingredient, blank=True)
-
-                                        def __str__(self):
-                                                return self.name
+class MenuItem(models.Model):
+    # ... existing fields ...
+        is_vegetarian = models.BooleanField(default=False)
