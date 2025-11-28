a622773691cc97e38ffec955156333e874f190c9
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-11-28 09:23:14 -0800

    61308

--- home/models.py
+++ home/models.py
@@ -1,31 +1,18 @@
 # home/models.py
 from django.db import models
 
-class Staff(models.Model):
-    CHEF = 'Chef'
-        WAITER = 'Waiter'
-            MANAGER = 'Manager'
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+        description = models.TextField()
 
-                ROLE_CHOICES = [
-                        (CHEF, 'Chef'),
-                                (WAITER, 'Waiter'),
-                                        (MANAGER, 'Manager'),
-                                            ]
+            def __str__(self):
+                    return self.name
 
-                                                first_name = models.CharField(max_length=100)
-                                                    last_name = models.CharField(max_length=100)
-                                                        role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-                                                            contact_email = models.EmailField()
+                    class MenuItem(models.Model):
+                        name = models.CharField(max_length=255)
+                            price = models.DecimalField(max_digits=8, decimal_places=2)
+                                is_available = models.BooleanField(default=True)
+                                    ingredients = models.ManyToManyField(Ingredient, blank=True)
 
-                                                                def __str__(self):
-                                                                        return f"{self.first_name} {self.last_name} - {self.role}"
-
-                                                                        # home/admin.py
-                                                                        from django.contrib import admin
-                                                                        from .models import Staff
-
-                                                                        @admin.register(Staff)
-                                                                        class StaffAdmin(admin.ModelAdmin):
-                                                                            list_display = ('first_name', 'last_name', 'role', 'contact_email')
-                                                                                list_filter = ('role',)
-                                                                                    search_fields = ('first_name', 'last_name', 'contact_email')
+                                        def __str__(self):
+                                                return self.name
