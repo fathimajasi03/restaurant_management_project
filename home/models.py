36eefb5d24bfe7f1c56aff76a96ccab4e2977c08
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-12-06 08:04:25 -0800

    61530

--- home/models.py
+++ home/models.py
@@ -1,6 +1,12 @@
 # home/models.py
 from django.db import models
 
-class MenuItem(models.Model):
-    # ... existing fields ...
-        is_vegetarian = models.BooleanField(default=False)
+class HolidayHours(models.Model):
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+        date = models.DateField()
+            opening_time = models.TimeField()
+                closing_time = models.TimeField()
+                    description = models.CharField(max_length=255, blank=True)
+
+                        def __str__(self):
+                                return f"{self.restaurant} - {self.date} ({self.description})"
