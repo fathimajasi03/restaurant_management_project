3a9bf8b0711fc65a2ed7e8d4a9903cdb5d76a0d9
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-12-10 08:56:21 -0800

    62739

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,4 @@
 # home/urls.py
-from django.urls import path
-from . import views
-
 urlpatterns = [
-    # ... existing paths ...
-        path('api/menu/active/', views.ActiveMenuItemsView.as_view(), name='active-menu-items'),
-        ]
+    path('api/menu/categories/', views.MenuCategoriesView.as_view(), name='menu-categories'),
+    ]
