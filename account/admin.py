4ef7f29e3e386f791676d7f4ab5d98202209df7e
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-12-10 08:42:26 -0800

    62741

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,10 @@
+# account/admin.py
 from django.contrib import admin
+from .models import LoyaltyProgram
 
-# Register your models here.
+class LoyaltyProgramAdmin(admin.ModelAdmin):
+    list_display = ['name', 'points_per_dollar_spent', 'is_active']
+        list_filter = ['is_active']
+            search_fields = ['name']
+
+            admin.site.register(LoyaltyProgram, LoyaltyProgramAdmin)
