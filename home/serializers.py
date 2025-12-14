25f3194f19ea91a92cc561fdbfa163c7fc61e1ba
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-12-14 08:19:06 -0800

    63056

--- home/serializers.py
+++ home/serializers.py
@@ -1,11 +1,8 @@
 # home/serializers.py
 from rest_framework import serializers
-from django.db.models import Count
-from .models import MenuCategory
+from .models import StaffShift
 
-class MenuCategoryWithCountSerializer(serializers.ModelSerializer):
-    item_count = serializers.IntegerField(read_only=True)
-
-        class Meta:
-                model = MenuCategory
-                        fields = ['id', 'name', 'item_count']
+class StaffShiftSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = StaffShift
+                    fields = '__all__'  # Includes all fields from StaffShift model
