0d6d55b814e912c69f74c215c8465d4fac614abe
fathimajasi03 <146059598+fathimajasi03@users.noreply.github.com>
2025-12-16 09:10:36 -0800

    63837

--- home/models.py
+++ home/models.py
@@ -1,19 +1,20 @@
 # home/models.py
 from django.db import models
-from django.utils import timezone
 
-class MenuItemManager(models.Manager):
-    def get_discounted_items(self):
-            """
-                    Returns queryset of menu items with active, non-expired discounts.
-                            """
-                                    return self.filter(
-                                                discount__is_active=True,
-                                                            discount__end_date__gte=timezone.now()
-                                                                    ).distinct()
-                                                                    
-                                                                    class MenuItem(models.Model):
-                                                                        # ... existing fields ...
-                                                                            discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True, blank=True)
-                                                                                
-                                                                                    objects = MenuItemManager()                     class Met
+DIETARY_CHOICES = [
+    ('VEGAN', 'Vegan'),
+        ('VEGETARIAN', 'Vegetarian'),
+            ('GLUTEN_FREE', 'Gluten-Free'),
+                ('HALAL', 'Halal'),
+                    ('NONE', 'None'),
+                    ]
+
+                    class MenuItem(models.Model):
+                        # ... existing fields ...
+                            dietary_preference = models.CharField(
+                                    max_length=20,
+                                            choices=DIETARY_CHOICES,
+                                                    default='NONE',
+                                                            blank=True,
+                                                                    null=True
+                                                                        )
